82d99680cd8bd9a10066d6f7539fdff59233cd02
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-30 00:30:19 +0700

    create user service

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface UserService {
+    Page<Users> getByPage(String name, String code, Pageable pageable);
+    List<Users> getByList();
+    Users delete(String id);
+}
+
