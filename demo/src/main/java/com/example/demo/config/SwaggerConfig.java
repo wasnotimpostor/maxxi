102cfe631220bf69db66465cfd81f5d11a1fb7cb
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-30 22:04:10 +0700

    create swagger config

---
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SecurityScheme(
+        name = "Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "Bearer"
+)
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(PathSelectors.regex("/error").negate())
+                .build();
+    }
+
+}
