c03e75685d3c123ad634101f1ec12924c5642c54
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-30 22:02:37 +0700

    update role repo

--- demo/src/main/java/com/example/demo/repository/RoleRepository.java
+++ demo/src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.RoleName;
 import com.example.demo.entity.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Roles, Integer> {
+    Optional<Roles> findByName(RoleName roleName);
 }
