3ecc3d4dcffe281ec71c81ed2b5ed4fb763c9991
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-30 09:37:50 +0700

    update user repository add findbyusername

--- demo/src/main/java/com/example/demo/repository/UsersRepository.java
+++ demo/src/main/java/com/example/demo/repository/UsersRepository.java
@@ -2,6 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, String> {
+    @Query(
+            value = "select u.*\n"+
+                    " from users as u\n"+
+                    " where (u.username = binary(?1)" +
+                    " or u.email = binary(?1)",
+            nativeQuery = true
+    )
+    Optional<Users> findByUsername(String username);
 }
