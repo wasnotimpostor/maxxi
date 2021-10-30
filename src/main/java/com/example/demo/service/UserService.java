2b2312f1cd42ff4caa0c7e6b40cca5a075118cfd
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-31 00:37:48 +0700

    update user service

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,13 +3,15 @@ package com.example.demo.service;
 import com.example.demo.entity.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserService {
     Page<Users> getByPage(String name, String code, Pageable pageable);
     List<Users> getByList();
     Users delete(String id);
-    Users save(String id, String request);
+    ResponseEntity<Map<String, Object>> save(String id, String request);
 }
 
