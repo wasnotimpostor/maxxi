010912dc769378978a30edf05536c81ee7c91b1c
Evan Mahendra <evan.mahendra@tsmid.com>
2021-10-31 16:30:39 +0700

    update user service add findbyid

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface UserService {
     Page<Users> getByPage(String name, String code, Pageable pageable);
     List<Users> getByList();
     Users delete(String id);
     ResponseEntity<Map<String, Object>> save(String id, String request);
+    ResponseEntity<Map<String, Object>> findById(String id);
 }
 
